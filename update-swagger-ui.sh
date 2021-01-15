aec0d8b3726f73b8b54f0230bc1b68d181e2c97e
Muli Yulzary <muliyul@gmail.com>
2021-01-15 08:38:31 +0200

    mkdir if not exists

--- update-swagger-ui.sh
+++ update-swagger-ui.sh
@@ -4,7 +4,7 @@ git sparse-checkout init --cone # to fetch only root files
 git sparse-checkout set dist # etc, to list sub-folders to checkout
 git read-tree -mu HEAD
 mkdir -p src/main/resources/static/swagger-ui
-mkdir tmp
+mkdir -p tmp
 cd tmp || exit 1
 mv tmp/dist/favicon-16x16.png src/main/resources/static/swagger-ui || exit 1
 mv tmp/dist/favicon-32x32.png src/main/resources/static/swagger-ui || exit 1
